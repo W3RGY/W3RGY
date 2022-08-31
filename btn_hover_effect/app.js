c28cc448bcc1266c7e2fcef2fe86033eddce10d8
Dima Osenkov <w3rgy1992@gmail.com>
2022-08-31 20:58:41 +0200

    Delete btn_hover_effect directory

--- btn_hover_effect/app.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('button')
-btn.addEventListener('mouseover', (e) => {
-	const x = e.pageX - btn.offsetLeft
-	const y = e.pageY - btn.offsetTop
-	btn.style.setProperty('--x', x + 'px')
-	btn.style.setProperty('--y', y + 'px')
-});
